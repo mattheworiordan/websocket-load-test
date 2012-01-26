7add2ad6be1cf87e1cb76b9ee21da2d1c0108747
Matthew O'Riordan <matthew.oriordan@gmail.com>
2012-01-26 23:44:51 +0000

    Server up SSL web socket

--- client.js
+++ client.js
@@ -5,7 +5,8 @@ var WebSocket = require('ws');
 // var ws = new WebSocket('ws://test-1884315901.eu-west-1.elb.amazonaws.com:80/');
 // var ws = new WebSocket('wss://easybacklog.com:443/');
 // var ws = new WebSocket('ws://easybacklog.com:80/');
-var ws = new WebSocket('wss://localhost:8043/');
+var ws = new WebSocket('wss://localhost:8003/');
+// var ws = new WebSocket('ws://localhost:8000/');
 ws.on('open', function() {
   sys.debug('Connection opened');
   sendMessage();
--- server.js
+++ server.js
@@ -1,17 +1,32 @@
-var sys = require('sys');
-var ws = require("ws").Server;
-
+var sys    = require('sys');
+var ws     = require("ws").Server;
+var fs     = require('fs');
 var server = new ws({ port: 8000 });
 
-server.on("connection", function(connection) {
-  connection.on("message", function(msg){
-    sys.debug('message received: ' + msg);
-    connection.send('message echo');
-  });
-});
+// add echo behaviour to standard non secure WS
+addEcho(server);
 
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('I\'m alive\n');
 }).listen(8001);
+
+var options = {
+  key: fs.readFileSync('/Users/matthew/Desktop/ssl/easybacklog.com.nopw.key', 'utf8'),
+  cert: fs.readFileSync('/Users/matthew/Desktop/ssl/easybacklog.com.pem', 'utf8')
+};
+var wss = require('https').createServer(options);
+wss.listen(8003);
+var wssServer = new ws({ server: wss });
+// add echo behaviour to SSL WS
+addEcho(wssServer);
+
+function addEcho(server) {
+  server.on("connection", function(connection) {
+    connection.on("message", function(msg){
+      sys.debug('message received: ' + msg);
+      connection.send('message echo');
+    });
+  });
+}
