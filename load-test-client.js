f257ee9d8b380004580156e3f721bcb063011745
Ubuntu <ubuntu@ip-10-56-162-140.eu-west-1.compute.internal>
2012-01-31 15:06:26 +0000

    Bug fix re: connections opened plus SSL verbosity

--- load-test-client.js
+++ load-test-client.js
@@ -14,7 +14,7 @@ var argv = require('optimist')
       .alias('r', 'requests').describe('r', 'number of total connection requests (defaults to 1,000)').default('r', 1000)
       .alias('h', 'host').describe('h', 'the hostname or IP address of the service (defaults to localhost)').default('h', 'localhost')
       .alias('p', 'port').describe('p', 'the port of the service (defaults to 8043)').default('p', 8043)
-      .alias('t', 'ramp_up_time').describe('t', 'time to take in seconds to ramp up connections (defaults to 10)').default('t', 10)
+      .alias('t', 'ramp_up_time').describe('t', 'time to take in seconds to ramp up connections (defaults to 10)').default('t', 0)
       .alias('s', 'no_ssl').describe('s', 'flag to disable SSL').default('s', false)
       .argv;
 
@@ -29,12 +29,14 @@ var currentConnections = 0,
     totalConnectionRequests = 0,
     startTime = new Date().getTime();
 
+console.log("Using SSL: " + (no_ssl ? 'No' : 'Yes'));
+
 // open a new connection to the server
 var openConnection = function() {
   var ws = new WebSocket((no_ssl ? 'ws' : 'wss') + '://' + host + ':' + port + '/');
+  currentConnections += 1;
   // on open connection, lets send a message to the server
   ws.on('open', function() {
-    currentConnections += 1;
     totalConnectionRequests += 1;
     ws.send('message');
   });
@@ -47,7 +49,6 @@ var openConnection = function() {
     } else {
       var timePassed = new Date().getTime() - startTime,
           rate = totalConnectionRequests / (timePassed / 1000);
-      console.log('Complete report ' + (no_ssl ? 'NOT using SSL' : 'using SSL'));
       console.log(totalConnectionRequests + ' connections in ' + (Math.round(timePassed/100)/10) + ' seconds.  Rate of ' + (Math.round(rate*10)/10) + ' connections per second');
       process.exit(0);
     }
